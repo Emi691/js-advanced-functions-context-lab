8982e594515c1b8c8b96dcbac9fdb2bbd013afd2
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-04-26 18:12:41 -0700

    commit tree 89bbfd58fde7de5b1136dc40bf80a716b47b5138

--- index.js
+++ index.js
@@ -1,4 +1,60 @@
 /* Your Code Here */
+function createEmployeeRecord(employeeArray){
+    const employeeRecord = {}
+    employeeRecord.firstName = employeeArray[0]
+    employeeRecord.familyName = employeeArray[1]
+    employeeRecord.title = employeeArray[2]
+    employeeRecord.payPerHour = employeeArray[3]
+    employeeRecord.timeInEvents = []
+    employeeRecord.timeOutEvents = []   
+    return employeeRecord   
+}
+
+function createEmployeeRecords(employeesArray){
+    const employeeRecords = employeesArray.map(employee => createEmployeeRecord(employee))
+    return employeeRecords
+}
+
+function createTimeInEvent(date){
+    const dateArray = date.split(' ')
+    const day = dateArray[0]
+    const hour = parseInt(dateArray[1])
+    this.timeInEvents.push({type: "TimeIn", hour: hour, date: day})
+    return this
+}
+
+function createTimeOutEvent(date){
+    const dateArray = date.split(' ')
+    const day = dateArray[0]
+    const hour = parseInt(dateArray[1])
+    this.timeOutEvents.push({type: "TimeOut", hour: hour, date: day})
+    return this
+}
+
+function hoursWorkedOnDate(date){
+    const clockIn = this.timeInEvents.find(timeInEvent => timeInEvent.date === date)
+    const clockOut = this.timeOutEvents.find(timeOutEvent => timeOutEvent.date === date)
+    const hoursWorked = (clockOut.hour - clockIn.hour)/100 
+    return hoursWorked
+}
+
+function wagesEarnedOnDate(date){
+    let hoursWorked = hoursWorkedOnDate.call(this, date)
+    let wagesEarned = hoursWorked * this.payPerHour
+    return wagesEarned
+}
+
+function findEmployeeByFirstName(srcArray, firstName){
+    const employee = srcArray.find(record => record.firstName === firstName)
+    return employee
+}
+
+function calculatePayroll(array){
+    const payroll = array.reduce((memo, record) => {
+        return memo + allWagesFor.call(record)
+    }, 0)
+    return payroll
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
